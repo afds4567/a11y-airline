32b440d47da573fcc90e8d13cdede3996bf30cdb
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-07-28 12:09:14 +0900

    웹 접근성~

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,21 +1,36 @@
-import React, { useState, MouseEvent } from "react";
+import React, { useState, MouseEvent, useEffect } from "react";
 import "./SpinButton.css";
 
 const SpinButton: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number>(1);
   const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
+  const [announceText, setAnnounceText] = useState<string>("성인 승객 1명");
 
   const increment = () => {
+    if (count === 3) return;
     setCount((prevCount) => prevCount + 1);
+    setAnnounceText(`성인 승객 추가 ${count + 1}`);
   };
 
   const decrement = () => {
+    if (count === 1) return;
     setCount((prevCount) => prevCount - 1);
+    setAnnounceText(`성인 승객 감소 ${count - 1}`);
   };
 
   const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
     setIsTooltipVisible(!isTooltipVisible);
   };
+
+  useEffect(() => {
+    // Announce text updates to screen readers
+    if (announceText) {
+      const liveRegion = document.getElementById("live-region");
+      if (liveRegion) {
+        liveRegion.innerText = announceText;
+      }
+    }
+  }, [announceText]);
 
   return (
     <section className="spinButtonContainer">
@@ -34,7 +49,11 @@ const SpinButton: React.FC = () => {
             )}
           </div>
         </div>
-        <button onClick={decrement} className="spinButton">
+        <button
+          onClick={decrement}
+          className="spinButton"
+          aria-label="성인 탑승자 한명 줄이기 버튼"
+        >
           -
         </button>
         <input
@@ -43,11 +62,21 @@ const SpinButton: React.FC = () => {
           readOnly
           className="spinButtonInput"
           value={count}
+          aria-label={`성인 ${count}명`}
         />
-        <button onClick={increment} className="spinButton">
+        <button
+          onClick={increment}
+          className="spinButton"
+          aria-label="성인 탑승자 한명 늘리기 버튼"
+        >
           +
         </button>
       </div>
+      <div
+        id="live-region"
+        aria-live="assertive"
+        style={{ position: "absolute", left: "-9999px" }}
+      ></div>
     </section>
   );
 };
